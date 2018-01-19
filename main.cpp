19359acc64785cbed9b508eab84b810b16bba002
user <toulmee@gmail.com>
2018-01-19 17:38:51 +0200

    Removed some debug code

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ int main(int argc, char **argv)
 {
     if (argc != 4)
     {
-        printf("Usage is: ./Resizer [filename] [scale factor] [number of threads]");
+        std::cout << "Usage is: ./Resizer [filename] [scale factor] [number of threads]" << std::endl;
         return -1;
     }
     std::string imgName = argv[1];
@@ -46,16 +46,12 @@ int main(int argc, char **argv)
 
     cv::Mat src = cv::imread(imgName.c_str(), CV_LOAD_IMAGE_COLOR);
     cv::Mat dst;
-    auto begin = std::chrono::high_resolution_clock::now();
-    cv::resize(src, dst, cv::Size(), scaleFactor, scaleFactor);
-    auto end = std::chrono::high_resolution_clock::now();
-    std::cout << "Single thread resize time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "\n";
 
-    cv::Mat dst2;
-    begin = std::chrono::high_resolution_clock::now();
-    mt_resize(src, dst2, scaleFactor, thread_num);
-    end = std::chrono::high_resolution_clock::now();
-    std::cout << "Multi thread resize time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "\n";
-    cv::imwrite("resullt.jpg", dst2);
+    auto begin = std::chrono::high_resolution_clock::now();
+    mt_resize(src, dst, scaleFactor, thread_num);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "Multithread resize time: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << std::endl;
+    cv::imwrite("resullt.jpg", dst);
     return 0;
 }
