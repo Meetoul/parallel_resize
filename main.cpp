7bed17bf5f89cea524e833bae8e04ce38764fe1e
user <toulmee@gmail.com>
2018-01-24 13:20:34 +0200

    Added ability to set a specific image size

--- main.cpp
+++ main.cpp
@@ -16,25 +16,24 @@ std::vector<char> read_raw(std::string &filename)
 
 int main(int argc, char **argv)
 {
-    if (argc != 4)
+    if (argc != 5)
     {
-        std::cout << "Usage is: ./Resizer [filename] [scale factor] [number of threads]" << std::endl;
+        std::cout << "Usage is: ./Resizer [filename] [width] [height] [number of threads]" << std::endl;
         return -1;
     }
     std::string imgName = argv[1];
-    double scaleFactor = std::stod(argv[2]);
-    int threadNum = std::stoi(argv[3]);
+    int width = std::stoi(argv[2]);
+    int height = std::stoi(argv[3]);
+    int threadNum = std::stoi(argv[4]);
     auto srcRaw = read_raw(imgName);
     std::vector<char> dstRaw;
     auto begin = std::chrono::high_resolution_clock::now();
-    resize(srcRaw, dstRaw, scaleFactor, threadNum);
+    resize(srcRaw, dstRaw, width, height, threadNum);
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << "Multithread resize time: "
               << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << std::endl;
     std::ofstream fout("result_new.jpg", std::ios::out | std::ios::binary);
     std::ostream_iterator<char> ofit(fout);
     std::copy(dstRaw.begin(), dstRaw.end(), ofit);
-    size_t rawSize = dstRaw.size();
-
     return 0;
 }
